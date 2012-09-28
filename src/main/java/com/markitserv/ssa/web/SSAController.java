4caadda1bb9532e3e97e50521bf7d099fed4d986
Roy Truelove <roy.truelove@markitserv.com>
2012-09-28 16:45:50 -0400

    Created a basic 'User' object for testing.

--- src/main/java/com/markitserv/ssa/web/SSAController.java
+++ src/main/java/com/markitserv/ssa/web/SSAController.java
@@ -1,27 +1,40 @@
 package com.markitserv.ssa.web;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.markitserv.ssa.data.User;
+
 @Controller
 @RequestMapping("/")
 
 public class SSAController {
+	
+	private Map<Long, User> users;
+	
+	public SSAController() {
+		buildFakeUsers();
+	}
 
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public @ResponseBody List<String> getUsers() {
-		
-		Stack<String> users = new Stack<String>();
-		users.push("Person A");
-		users.push("Person B");
-		
-		return users;
-		
+	public @ResponseBody Collection<User> getUsers() {
+		return users.values();
 	}
+	
+	private void buildFakeUsers() {
+		users = new HashMap<Long, User>();
+		
+		User user1 = new User(1, "Roy", "Truelove");
+		User user2 = new User(2, "Jane", "Truelove");
+		
+		users.put(1l, user1);
+		users.put(2l, user2);
+	}
+
 }
