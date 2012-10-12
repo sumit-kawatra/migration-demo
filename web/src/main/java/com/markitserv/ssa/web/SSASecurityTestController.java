3de00ab26663f026aabc36feae78e5e152f4af8c
swati.choudhari <swati.choudhari@usmservdpc007.markit.partners>
2012-10-12 15:39:07 -0400

    User Authentication.

---
+++ web/src/main/java/com/markitserv/ssa/web/SSASecurityTestController.java
@@ -0,0 +1,28 @@
+package com.markitserv.ssa.web;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class SSASecurityTestController{
+	
+	Logger log = LoggerFactory.getLogger(SSASecurityTestController.class);
+	
+	
+	@RequestMapping(value = "/secured", method = RequestMethod.GET) 
+    public @ResponseBody String secured() {
+		return "SECURED: " + new Date(); 
+    }
+	
+}
