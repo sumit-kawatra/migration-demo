140fa6049ebef8fe4dfc406fd0ce70c49aecdce8
swati.choudhari <swati.choudhari@usmservdpc007.markit.partners>
2012-12-12 10:14:55 -0500

    Adding method level Spring security.

---
+++ web/src/main/java/com/markitserv/ssa/security/MyCustomSecurityExpressionRoot.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.markitserv.ssa.security;
+
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionOperations;
+import org.springframework.security.core.Authentication;
+
+/**
+ * @author swati.choudhari
+ *
+ */
+
+public class MyCustomSecurityExpressionRoot extends SecurityExpressionRoot implements MethodSecurityExpressionOperations {
+	
+	private Object filterObject;
+	private Object returnObject;
+	private Object target;
+	
+	public Object getFilterObject() {
+		return filterObject;
+	}
+
+	public void setFilterObject(Object filterObject) {
+		this.filterObject = filterObject;
+	}
+
+	public Object getReturnObject() {
+		return returnObject;
+	}
+
+	public void setReturnObject(Object returnObject) {
+		this.returnObject = returnObject;
+	}
+
+	public Object getTarget() {
+		return target;
+	}
+
+	public void setTarget(Object target) {
+		this.target = target;
+	}
+	
+	public MyCustomSecurityExpressionRoot(Authentication authentication) {
+		super(authentication);
+	}
+	
+	public boolean myCustomMethodCheck() {
+		System.out.println("**Passing myCustomMethodCheck Successfully**");
+		// complex logic will go here
+		return true;
+    }
+
+	@Override
+	public Object getThis() {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+	
+}
--- web/src/main/java/com/markitserv/ssa/web/SSASecurityTestController.java
+++ web/src/main/java/com/markitserv/ssa/web/SSASecurityTestController.java
@@ -1,16 +1,18 @@
 package com.markitserv.ssa.web;
 
+import java.security.Principal;
 import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
 
 
 @Controller
@@ -18,8 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SSASecurityTestController{
 	
 	Logger log = LoggerFactory.getLogger(SSASecurityTestController.class);
-	
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET) 
     public @ResponseBody String secured() {
 		System.out.println("in SSASecurityTestController");
@@ -27,9 +27,16 @@ public class SSASecurityTestController{
     }
 	
 	@RequestMapping(value = "/secured", method = RequestMethod.POST)
-    public @ResponseBody String securedPOST() {
+	public @ResponseBody String securedPOST() {
 		System.out.println("in SSASecurityTestController");
-		return "SECURED POST: " + new Date(); 
+		return "SECURED POST: " + new Date();
     }
 	
+	@PreAuthorize("myCustomMethodCheck()")
+	//@PreAuthorize("hasRole('ROLE_USER')")
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public @ResponseBody String elTestSayHello(Principal principal, HttpServletRequest request) {
+		return "SECURED GET :  Welcome " + principal.getName() + new Date() + request.getRequestURL();
+		
+    }
 }
