13c13ebec64a1fd862f3b8e58cc299565960cdd2
kiran.gogula <kiran.gogula@usmservdpc011.markit.partners>
2013-01-16 16:19:44 -0500

    made changes for hashcode, equals and toString methods

--- web/src/main/java/com/markitserv/hawthorne/types/TradingRequestStatus.java
+++ web/src/main/java/com/markitserv/hawthorne/types/TradingRequestStatus.java
@@ -1,5 +1,10 @@
 package com.markitserv.hawthorne.types;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import com.markitserv.mwws.Type;
 
 public class TradingRequestStatus extends Type {
@@ -28,6 +33,33 @@ public class TradingRequestStatus extends Type {
 	}
 	public void setStatus(String status) {
 		this.status = status;
+	}
+	
+	@Override
+	public int hashCode() {
+		   return HashCodeBuilder.reflectionHashCode(id);
+	 }
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+		TradingRequestStatus tradeRequestStatus = (TradingRequestStatus)obj;
+		return  new EqualsBuilder().appendSuper(super.equals(obj))
+				.append(id, tradeRequestStatus.id).isEquals();
+	}
+
+	
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
 	}
 
 }
