58d4748f61977eb820f4d14364fee0252a7db765
Roy Truelove <roy.truelove@markitserv.com>
2013-06-25 12:58:44 -0400

    Moved everything

--- web/src/main/scripts/start.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-# import environment-specific variables
-require 'setup-env.pl';
-
-exec ("(java -classpath hawthorne-server:$JAVA_HOME/lib/tools.jar -DNAME=hawthorne-server EmbeddedJettyServer $HTTP_PORT $JETTY_TMP_DIR &) &>> /opt/logs/hawthorne/master.out");
