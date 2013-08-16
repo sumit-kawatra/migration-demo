9c2ad7dc661a148ee9a893d7f704f944ff936fe3
Roy Truelove <roy.truelove@markitserv.com>
2013-08-16 13:37:40 -0400

    Some formatting change, nothing material

--- core/src/main/java/com/markitserv/msws/web/UploadedFile.java
+++ core/src/main/java/com/markitserv/msws/web/UploadedFile.java
@@ -6,33 +6,40 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * Encapsulates an uploaded file.
+ * 
  * @author roy.truelove
- *
+ * 
  */
 public class UploadedFile {
-	
+
 	private InputStream inputStream;
 	private String contentType;
 	private String fileName;
-	
+
 	public InputStream getInputStream() {
 		return inputStream;
 	}
+
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
 	}
+
 	public String getContentType() {
 		return contentType;
 	}
+
 	public void setContentType(String contentType) {
 		this.contentType = contentType;
 	}
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
+
 	public String getFileName() {
 		return fileName;
 	}
+
 	public String getFileExtention() {
 		return StringUtils.substringAfterLast(this.getFileName(), ".");
 	}
