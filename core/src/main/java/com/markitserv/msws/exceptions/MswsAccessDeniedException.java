31cf5c30954c854f0cacc5beca19df1f4b411550
Roy Truelove <roy.truelove@markitserv.com>
2013-09-10 10:43:49 -0400

    Added MSWS Access denied exception.  Should go down to the user

---
+++ core/src/main/java/com/markitserv/msws/exceptions/MswsAccessDeniedException.java
@@ -0,0 +1,34 @@
+package com.markitserv.msws.exceptions;
+
+public class MswsAccessDeniedException extends MswsException {
+
+	public MswsAccessDeniedException() {
+		super();
+	}
+
+	public MswsAccessDeniedException(String format, Object... args) {
+		super(format, args);
+	}
+
+	public MswsAccessDeniedException(String format, Throwable cause,
+			Object... args) {
+		super(format, cause, args);
+	}
+
+	public MswsAccessDeniedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public MswsAccessDeniedException(String message) {
+		super(message);
+	}
+
+	public MswsAccessDeniedException(Throwable cause) {
+		super(cause);
+	}
+
+	public static MswsAccessDeniedException standardException(String user, String action) {
+		return new MswsAccessDeniedException(
+				"User '%s' cannot perform action '%s'");
+	}
+}
