850ed5036fd28ad69cee2e331ebcc78a4c72826c
Roy Truelove <roy.truelove@markitserv.com>
2013-09-23 16:16:12 -0400

    Interim Commit

--- core/src/main/java/com/markitserv/msws/web/JsonpFilter.java
+++ core/src/main/java/com/markitserv/msws/web/JsonpFilter.java
@@ -18,6 +18,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ * This is a TEMPORARY CLASS until we're sure that the client no longer is using
+ * JSONp
+ * 
+ * @author roy.truelove
+ * 
+ */
 public class JsonpFilter implements Filter {
 
 	Logger log = LoggerFactory.getLogger(this.getClass());
@@ -28,86 +35,18 @@ public class JsonpFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-		log.info("jsonp filter");
-		System.out.println("JSPON");
-		
-		// Since this filter always returns json P..
+		Map<String, String[]> parms = request.getParameterMap();
 
-		@SuppressWarnings("unchecked")
-		Map<String, String[]> parms = httpRequest.getParameterMap();
-
-		// TODO make this DEBUG
-		log.info("Query String: " + httpRequest.getQueryString());
-		System.out.println("Query String: " + httpRequest.getQueryString());
-		
-		OutputStream respOutputStream = httpResponse.getOutputStream();
-
-		GenericResponseWrapper respWrapper = new GenericResponseWrapper(
-				httpResponse);
-		
-		byte[] respData; 
-		
 		if (parms.containsKey("callback")) {
-			
-			String callbackParam = parms.get("callback")[0];
-
-			if (log.isDebugEnabled())
-				log.debug("Wrapping response with JSONP callback '"
-						+ callbackParam + "'");
-
-			// User can override the type of the response. Only really used for
-			// zenoss, which
-			// doesn't like JSON.
-
-			chain.doFilter(request, respWrapper);
-			
-			StringBuffer sb = new StringBuffer();
-
-			sb.append(new String(callbackParam + "("));
-			sb.append(new String(respWrapper.getData()));
-			sb.append(new String(");"));
-
-			String output = sb.toString();
-			respData = output.getBytes();
-			
-			respWrapper.setContentType("application/javascript; charset=UTF-8");
-
-		} else {
-			log.info("No 'callback' param so not wrapping in jsonp");
-			chain.doFilter(request, respWrapper);
-			respData = respWrapper.getData();
-			// if there's no wrapper, it's plain ol JSON
-			respWrapper.setContentType("application/json; charset=UTF-8");
+			throw new ServletException("JSONP is no longer supported.");
 		}
 		
-		respWrapper.setContentType(mungeContentType(httpRequest, respWrapper));
-		
-		respOutputStream.write(respData);
-		respOutputStream.close();
+		chain.doFilter(request, response);
 	}
 
-	// Overrides content type as needed
-	private String mungeContentType(HttpServletRequest req, HttpServletResponse resp) {
-		
-		String userAgent = req.getHeader("User-Agent");
-		String method = req.getMethod();
-		
-		String contentType = resp.getContentType();
-
-		boolean isIe = StringUtils.contains(userAgent, "MSIE");
-		boolean isPost = method.equalsIgnoreCase("POST");
-		
-		if (isIe && isPost) {
-			// Posts for IE require HTML type.
-			contentType = ("text/html; charset=UTF-8");
-		}
-		
-		return contentType;
-	}
-
+	@Override
 	public void destroy() {
+		// Don't need to destroy anything
 	}
 }
